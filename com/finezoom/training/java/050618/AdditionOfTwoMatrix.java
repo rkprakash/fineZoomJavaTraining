4e0ee6244e45f70c4823d5bd26be0349030dc3fb
rkprakash <39897172+rkprakash@users.noreply.github.com>
2018-06-06 21:16:13 +0530

    commit from Addition of two cross matrix

---
+++ com/finezoom/training/java/050618/AdditionOfTwoMatrix.java
@@ -0,0 +1,39 @@
+package com.finezoom.training.java.050618;
+
+import java.util.Scanner;
+
+/**
+ * Addition of two cross two matrix
+ *
+ */
+class AdditionOfTwoMatrix {
+	public static void main(String args[]) {
+		int[][] inputValuea = new int[2][2];
+		int[][] inputValueb = new int[2][2];
+		int[][] sum = new int[2][2];
+		int len = inputValuea.length;
+		int len1 = inputValueb.length;
+		int i, j = 0;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the First Matrix");
+		for (i = 0; i < len; i++) {
+			for (j = 0; j < len; j++) {
+				inputValuea[i][j] = scan.nextInt();
+			}
+		}
+		System.out.println("Enter the Second Matrix");
+		for (i = 0; i < len1; i++) {
+			for (j = 0; j < len1; j++) {
+				inputValueb[i][j] = scan.nextInt();
+			}
+		}
+		System.out.println("Output of program");
+		for (i = 0; i < len1; i++) {
+			for (j = 0; j < len1; j++) {
+				sum[i][j] = inputValuea[i][j] + inputValueb[i][j];
+				System.out.println(sum[i][j]);
+			}
+		}
+
+	}
+}
